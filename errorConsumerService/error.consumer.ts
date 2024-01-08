390f67e78505d80575db509f312e2a4d48f21422
GeorgeJose <subingeorge027@gmail.com>
2024-01-09 00:13:49 +0530

    created error consumer

---
+++ errorConsumerService/error.consumer.ts
@@ -0,0 +1,28 @@
+import amqp from 'amqplib';
+
+async function consumeMessages() {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel : amqp.Channel  = await connection.createChannel();
+
+  await channel.assertExchange('logExchange', 'direct');
+ 
+  const q = await channel.assertQueue('InfoQueue');
+
+  await channel.bindQueue(q.queue, 'logExchange', 'Info');
+
+  channel.consume(q.queue, (msg) => {
+    if (msg !== null && msg.content) {
+      try {
+        const data = JSON.parse(msg.content.toString());
+        console.log('Received message:', data);
+        channel.ack(msg);
+      } catch (error) {
+        console.error('Error parsing message content:', error);
+        console.log('Raw message content:', msg.content.toString());
+      }
+    }
+  });
+}
+
+
+consumeMessages()
