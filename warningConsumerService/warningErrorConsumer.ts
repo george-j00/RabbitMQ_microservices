975af44d3e6a5a631e04be77422999dc0b352887
GeorgeJose <subingeorge027@gmail.com>
2024-01-09 00:12:38 +0530

    created consumers and producers

---
+++ warningConsumerService/warningErrorConsumer.ts
@@ -0,0 +1,29 @@
+import amqp from 'amqplib';
+
+async function consumeMessages() {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel : amqp.Channel  = await connection.createChannel();
+
+  await channel.assertExchange('logExchange', 'direct');
+ 
+  const q = await channel.assertQueue('WarningErrorQueue');
+
+  await channel.bindQueue(q.queue, 'logExchange', 'warning');
+  await channel.bindQueue(q.queue, 'logExchange', 'error');
+    
+  channel.consume(q.queue, (msg) => {
+    if (msg !== null && msg.content) {
+      try {
+        const data = JSON.parse(msg.content.toString());
+        console.log('Received message:', data);
+        channel.ack(msg);
+      } catch (error) {
+        console.error('Error parsing message content:', error);
+        console.log('Raw message content:', msg.content.toString());
+      }
+    }
+  });
+}
+
+
+consumeMessages()
